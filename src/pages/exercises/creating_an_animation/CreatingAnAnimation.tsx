2b5d0d1bb30b636ed0e260802cf0553ba7227dd2
AndDev <23031429@itcelaya.edu.mx>
2025-10-31 16:49:13 -0600

    style: update layout structure in CreatingAnAnimation component

--- src/pages/exercises/creating_an_animation/CreatingAnAnimation.tsx
+++ src/pages/exercises/creating_an_animation/CreatingAnAnimation.tsx
@@ -65,7 +65,7 @@ const CreatingAnAnimation = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-8 px-8 max-w-4xl w-full">
+    <div className="flex flex-col items-center justify-center gap-8 px-4 max-w-4xl w-full">
       <section className="text-center space-y-2">
         <Title>Creando una animación</Title>
         <Paragraph>
@@ -87,7 +87,8 @@ const CreatingAnAnimation = () => {
           />
         </div>
       </section>
-      <div className="flex gap-4 justify-end w-full">
+
+      <section className="flex gap-4 justify-end w-full">
         <div className="relative">
           <input
             type="number"
@@ -100,7 +101,7 @@ const CreatingAnAnimation = () => {
             ms
           </span>
         </div>
-      </div>
+      </section>
 
       <section className="flex gap-4 sm:justify-end justify-between w-full">
         <Button onClick={handleRunAnimation} variant="primary">
@@ -189,7 +190,8 @@ const CreatingAnAnimation = () => {
           />
         </div>
       </section>
-      <div className="flex gap-4 justify-end w-full">
+
+      <section className="flex gap-4 justify-end w-full">
         <div className="relative">
           <input
             type="number"
@@ -202,7 +204,7 @@ const CreatingAnAnimation = () => {
             ms
           </span>
         </div>
-      </div>
+      </section>
 
       <section className="flex gap-4 sm:justify-end justify-between w-full">
         <Button onClick={handleRunAnimation} variant="primary">
